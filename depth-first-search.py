dfca19f89cb43f6bceb3f8383df32578cd99bbdd
Andrew Makarov <wilmanwdk@hotmail.com>
2019-03-16 21:59:31 +0200

    add DFS (2nd lab); structure edits for BFS

--- depth-first-search.py
+++ depth-first-search.py
@@ -27,56 +27,13 @@ def any_unused_left(nodes):
         if node.state == 0:
             return True
     return False
-# Найти в заданном графе кратчайшие пути из заданной вершины
-# до всех остальных вершин с помощью поиска в ширину
-
-nodes = []
-edges = []
-
-Q = []
-
-for i in range(ord('A'), ord('J') + 1):
-    node = Node(chr(i))
-    nodes.append(node)
-
-edges.append(Edge(nodes[0], nodes[1]))
-edges.append(Edge(nodes[1], nodes[2]))
-edges.append(Edge(nodes[2], nodes[3]))
-edges.append(Edge(nodes[3], nodes[4]))
-edges.append(Edge(nodes[0], nodes[9]))
-edges.append(Edge(nodes[9], nodes[4]))
-edges.append(Edge(nodes[0], nodes[5]))
-edges.append(Edge(nodes[5], nodes[6]))
-edges.append(Edge(nodes[5], nodes[7]))
-edges.append(Edge(nodes[7], nodes[8]))
-edges.append(Edge(nodes[8], nodes[9]))
-
-nodes[0].distance = 0
-Q.append(nodes[0])
-
-while len(Q) > 0:
-    current = Q.pop(0)
-    for adjacent in adjacent_nodes(current, edges):
-        if adjacent.state == 0:
-            adjacent.state = 1
-            adjacent.parent = current
-            adjacent.distance = current.distance + 1
-            Q.append(adjacent)
-    current.state = 2
-
-print("=== 1 ===")
-for node in nodes:
-    print(f"{node}: {node.distance}")
-print()
 
 # Найти в заданном графе количество и состав компонент связности
-# с помощью поиска в ширину
+# с помощью поиска в глубину
 
 nodes = []
 edges = []
 components = []
-
-Q = []
 
 for i in range(1, 9):
     node = Node(i)
@@ -95,6 +52,8 @@ edges.append(Edge(nodes[4], nodes[5]))
 edges.append(Edge(nodes[4], nodes[7]))
 edges.append(Edge(nodes[5], nodes[7]))
 
+Q = []
+
 component = []
 while any_unused_left(nodes):
     root = None
@@ -102,21 +61,18 @@ while any_unused_left(nodes):
         if node.state == 0:
             root = node
             break
-
     Q.append(root)
-
     while len(Q) > 0:
-        current = Q.pop(0)
-        component.append(current)
-        for adjacent in adjacent_nodes(current, edges):
-            if adjacent.state == 0:
-                adjacent.state = 1
-                adjacent.parent = current
+        current = Q.pop()
+        if current.state == 0:
+            current.state = 2
+            component.append(current)
+            for adjacent in adjacent_nodes(current, edges):
                 Q.append(adjacent)
-        current.state = 2
     components.append(component)
     component = []
 
-print("=== 2 ===")
+print("=== 1 ===")
 for component in components:
     print(", ".join(map(str, component)))
+print()
